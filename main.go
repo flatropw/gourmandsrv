90944daaed0fda72b3f6a9d6a88737dc0f01d915
root <changelater@mail.com>
2019-12-04 20:26:39 +0300

    add Visitor struct type

--- main.go
+++ main.go
@@ -4,6 +4,14 @@ import (
 	"log"
 	"net/http"
 )
+type (
+	Visitor struct {
+		Host       string
+		UserAgent  string
+		RequestUri string
+		Headers    http.Header
+	}
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
