70376afe022ffa71131fee90391e06d9286735f1
root <changelater@mail.com>
2019-12-04 20:30:17 +0300

    add output to browser about Visitor in JSON format

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -15,7 +16,17 @@ type (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
+		jsonVisitorData, err := json.Marshal(
+			Visitor{
+				r.Host,
+				r.Header.Get("User-Agent"),
+				r.URL.Path,
+				r.Header,
+			})
+		if err != nil {
+			panic(err)
+		}
+		_, err = w.Write(jsonVisitorData)
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
